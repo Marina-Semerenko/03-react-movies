79d0c01d6ca5f4887a5862a432b1a2969f888811
Marina <marina.miram@gmail.com>
2026-04-01 22:02:42 +0300

    .

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -5,11 +5,14 @@ interface MovieResponse {
     results: Movie[];
     page: number;
     total_pages: number;
-    total_result: number;
+    total_results: number;
 }
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3/search/movie"; 
 export const searchMovies = async (query: string): Promise<Movie[]> => {
+    if (!TMDB_TOKEN) {
+        throw new Error("Mising TMDB_TOKEN in environment variables");
+    }
   const response = await axios.get<MovieResponse>(BASE_URL, {
         params: {
             query,
